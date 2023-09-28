2a9a433cf230d64ed6a3b7c7316e4e4017c5570d
S8Vega <bsebastianvg18@gmail.com>
2023-09-28 06:59:52 -0500

    update service controller

--- employee/src/main/java/com/thechaoscompany/employee/controller/EmployeeController.java
+++ employee/src/main/java/com/thechaoscompany/employee/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.thechaoscompany.employee.controller;
 import com.thechaoscompany.employee.entity.EmployeeEntity;
 import com.thechaoscompany.employee.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,19 +11,29 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 @RestController
 @RequestMapping("/employees")
 @RequiredArgsConstructor
+@Log4j2
 public class EmployeeController {
     private final EmployeeRepository repository;
 
     @GetMapping("/{id}")
     public Mono<EmployeeEntity> getEmployeeById(@PathVariable long id) {
-        return Mono.just(repository.findById(id).orElseThrow());
+        log.info("getEmployeeById: {}", id);
+        EmployeeEntity employee = repository.findById(id).orElseThrow();
+        return Mono.just(employee);
     }
 
     @GetMapping
     public Flux<EmployeeEntity> getAllEmployees() {
-        return Flux.fromIterable(repository.findAll());
+        log.info("getAllEmployees");
+        List<EmployeeEntity> employees = StreamSupport.stream(repository.findAll().spliterator(), false)
+                .collect(Collectors.toList());
+        return Flux.fromIterable(employees);
     }
 }
